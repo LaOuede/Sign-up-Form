66e1af65a6798752be2eb04dd4413c81a82766f2
Gwenola Le Roux <gwenola.leroux@gmail.com>
2024-06-24 09:45:49 -0400

    Sign-up Form : done

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
-# Sign-up-Form
+<p align="center">
+  <img src="https://github.com/LaOuede/Landing-page/blob/main/images/signup-form.png" width="90"/>
+</p>
+
+<h1 align=center>Sign-up Form</h1>
+
+<p align="center">
+  <a href="https://laouede.github.io/Sign-up-Form/">Live demo here</a>
+</p>
+
+<p align=center>
+The Sign-Up Form project in web development involves creating a form that allows users to sign up for a service or register an account. This project focuses on form validation, user experience, and styling to ensure that the form is both functional and visually appealing.
+</p>
+
+---
+
+<h3 align="left">🎯 Objective</h3>
+
+<p align=left>
+The development process for this sign-up form involves planning the layout, designing the visuals, and coding the form using HTML and CSS. JavaScript is used for form validation to ensure all fields are filled out correctly. The purpose is to create a user-friendly form that makes the registration process smooth and efficient.
+</p>
+
+---
+
+<h3 align="left">📚 What I've learned</h3>
+
+I've acquired a lot of basic knowledge with :
+- **HTML**: Structuring web content effectively using semantic HTML tags.
+- **CSS**: Styling web pages with CSS, including Flexbox for layout.
+
+---
+
+<h3 align="left">⚠️ Notice</h3>
+<p>Please note that this landing page is currently not responsive. It may not display correctly on different devices or screen sizes. For the best experience, please view this page on a desktop computer.</p>
+
+---
+
+<h3 align="left">🖥 Display</h3>
+
+
+---
+
+<div align="center">
+
+Go to [The Odin Project](https://www.theodinproject.com/) to discover the course ! 👈
+</div>
---
+++ script.js
@@ -0,0 +1,62 @@
+const form = document.querySelector('form');
+
+const regexName = /^[A-Za-zÀ-ÖØ-öø-ÿ' -]+$/;
+const regexPwd = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+
+const validateFirstname = (input, regex) => {
+	if (!regex.test(input.value)) {
+		alert('First name can only contain letters, accents, apostrophes, and hyphens.');
+		input.value = '';
+		return false;
+	}
+	return true;
+}
+
+const validateLastname = (input, regex) => {
+	if (!regex.test(input.value)) {
+		alert('Last name can only contain letters, accents, apostrophes, and hyphens.');
+		input.value = '';
+		return false;
+	}
+	return true;
+}
+
+const validatePassword = (input, regex) => {
+	if (!regex.test(input.value)) {
+		alert('Password must be at least 8 characters long, contain at least one \
+			uppercase letter, one lowercase letter, one number, and one special character.');
+		input.value = '';
+		return false;
+	}
+	return true;
+}
+
+const validatePasswordMatch = (pwd1, pwd2) => {
+	if (pwd1.value != pwd2.value) {
+		alert('Passwords do not match.');
+		pwd1.value = '';
+		pwd2.value = '';
+		return false;
+	}
+	return true;
+}
+
+form.addEventListener('submit', (event) => {
+	event.preventDefault();
+
+	const firstname = document.getElementById('firstname');
+	const lastname = document.getElementById('lastname');
+	const pwd = document.getElementById('password');
+	const pwdVal = document.getElementById('password_validation');
+
+	const firstnameValidation = validateFirstname(firstname, regexName);
+	const lastnameValidation = validateLastname(lastname, regexName);
+	const passwordValidation = validatePassword(pwd, regexPwd);
+	const passwordMatch = validatePasswordMatch(pwd, pwdVal);
+
+	if (firstnameValidation && lastnameValidation
+		&& passwordValidation && passwordMatch) {
+		form.reset();
+		alert('Your account have been successfully created');
+	}
+});
